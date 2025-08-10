f214454a7c052f1792381885d519e71dcb8ef054
Gaurav-8941 <gauravchavan28123@gmail.com>
2025-08-10 13:56:43 +0530

    upadte in main files

--- app/src/main/java/com/example/disastermanagementandalertsystem/MainActivity.java
+++ app/src/main/java/com/example/disastermanagementandalertsystem/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
+// commit here so you can see this in oyouaf adaljad
 //main code from gavaruv
 public class MainActivity extends AppCompatActivity {
     ListView language;
